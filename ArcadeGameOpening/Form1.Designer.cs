a8b2181da046320ed035919edba49629502b4f61
valemont800 <valemont800@SDS-L-D41HC63>
2023-02-09 14:09:24 -0500

    Add project files.

---
+++ ArcadeGameOpening/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows.Forms;
+
+namespace ArcadeGameOpening
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PlayButton.Font = new System.Drawing.Font("MS PGothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PlayButton.Location = new System.Drawing.Point(147, 97);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(142, 38);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Play Game ?";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayGame_Click);
+            this.PlayButton.MouseLeave += new System.EventHandler(this.PlayGame_MouseLeave);
+            this.PlayButton.MouseHover += new System.EventHandler(this.PlayGame_MouseHover);
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Location = new System.Drawing.Point(289, 230);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(0, 13);
+            this.countLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(450, 329);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.PlayButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arcade Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playGame;
+
+        public Form1(Button PlayButton)
+        {
+            this.playGame = PlayButton;
+        }
+
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Label countLabel;
+    }
+}
+
---
+++ ArcadeGameOpening/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace ArcadeGameOpening
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void PlayGame_MouseHover(object sender, EventArgs e)
+        {
+            PlayButton.ForeColor = Color.Red;
+        
+        }
+
+        private void PlayGame_MouseLeave(object sender, EventArgs e)
+        {
+            PlayButton.ForeColor = Color.White;
+        }
+
+        private void PlayGame_Click(object sender, EventArgs e)
+        {
+            Refresh();
+
+            PlayButton.Text = "Alright";
+
+            SoundPlayer alertplayer = new SoundPlayer(Properties.Resources.gameCube);
+            alertplayer.Play();
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting . .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting . . .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting . .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting . . .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting . .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Text = "Starting . . .";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.ForeColor = Color.White;
+            PlayButton.Text = "Play !";
+
+            Refresh();
+            Thread.Sleep(500);
+
+            PlayButton.Visible = false;
+
+            
+
+        }
+    }
+}
